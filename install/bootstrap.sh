69c2f5377cde08cb8067c1be190f4b8923544b73
Priyobroto Kar <104967639+PriyobrotoKar@users.noreply.github.com>
2024-12-26 02:41:16 +0530

    fixed the order of installation

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -1,12 +1,17 @@
-#!bin/bash
-
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#!/bin/bash
 
 # Install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo >> /Users/test/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/test/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+
 # Install dependencies from BrewFile
-brew bundle install --file=install/Brewfile
+brew bundle install --file=./Brewfile
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Create symlinks for dotfiles
