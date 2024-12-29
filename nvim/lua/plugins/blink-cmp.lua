c57799ae367e42550195b120ec63eeb82cab5264
Priyobroto Kar <priyobrotokar@gmail.com>
2024-12-30 01:46:31 +0530

    Fix blink.cmp keymaps and copilot interference

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -1,103 +1,128 @@
 return {
   "saghen/blink.cmp",
-  -- optional: provides snippets for the snippet source
-  dependencies = "rafamadriz/friendly-snippets",
-
-  -- use a release tag to download pre-built binaries
-  version = "*",
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
+  version = not vim.g.lazyvim_blink_main and "*",
+  build = vim.g.lazyvim_blink_main and "cargo build --release",
+  -- opts_extend = {
+  --   "sources.completion.enabled_providers",
+  --   "sources.compat",
+  --   "sources.default",
+  -- },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+  },
+  event = "InsertEnter",
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = "default" },
-
+    snippets = {
+      expand = function(snippet, _)
+        return LazyVim.cmp.expand(snippet)
+      end,
+    },
     appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- Will be removed in a future release
-      use_nvim_cmp_as_default = true,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
+      -- sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release, assuming themes add support
+      use_nvim_cmp_as_default = false,
+      -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- adjusts spacing to ensure icons are aligned
       nerd_font_variant = "mono",
     },
+    completion = {
+      accept = {
+        -- experimental auto-brackets support
+        auto_brackets = {
+          enabled = true,
+        },
+      },
+      menu = {
+        draw = {
+          treesitter = { "lsp" },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+      ghost_text = {
+        enabled = vim.g.ai_cmp,
+      },
+    },
 
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    -- experimental signature help support
+    -- signature = { enabled = true },
+
     sources = {
+      -- adding any nvim-cmp sources here will enable them
+      -- with blink.compat
+      compat = {},
       default = { "lsp", "path", "snippets", "buffer" },
+      providers = {
+        copilot = {
+          name = "copilot",
+          enabled = false,
+        },
+      },
+      cmdline = {},
+    },
+
+    keymap = {
+      preset = "enter",
+      ["<C-y>"] = { "select_and_accept" },
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<C-k>"] = { "select_prev", "fallback" },
     },
   },
-}
+  ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
+  config = function(_, opts)
+    -- add ai_accept to <Tab> key
+    if not opts.keymap["<Tab>"] then
+      if opts.keymap.preset == "super-tab" then -- super-tab
+        opts.keymap["<Tab>"] = {
+          require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
+          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+          "fallback",
+        }
+      else -- other presets
+        opts.keymap["<Tab>"] = {
+          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+          "fallback",
+        }
+      end
+    end
 
--- return {
---   "hrsh7th/nvim-cmp",
---   version = false, -- last release is way too old
---   event = "InsertEnter",
---   dependencies = {
---     "hrsh7th/cmp-nvim-lsp",
---     "hrsh7th/cmp-buffer",
---     "hrsh7th/cmp-path",
---   },
---   -- Not all LSP servers add brackets when completing a function.
---   -- To better deal with this, LazyVim adds a custom option to cmp,
---   -- that you can configure. For example:
---   --
---   -- ```lua
---   -- opts = {
---   --   auto_brackets = { "python" }
---   -- }
---   -- ```
---   opts = function()
---     vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
---     local cmp = require("cmp")
---     local defaults = require("cmp.config.default")()
---     local auto_select = true
---     return {
---       auto_brackets = {}, -- configure any filetype to auto add brackets
---       completion = {
---         completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
---       },
---       preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
---       mapping = cmp.mapping.preset.insert({
---         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
---         ["<C-f>"] = cmp.mapping.scroll_docs(4),
---         ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
---         ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
---         ["<C-Space>"] = cmp.mapping.complete(),
---         ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
---         ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
---         ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
---         ["<C-CR>"] = function(fallback)
---           cmp.abort()
---           fallback()
---         end,
---         ["<tab>"] = function(fallback)
---           return LazyVim.cmp.map({ "snippet_forward", "ai_accept" }, fallback)()
---         end,
---       }),
---       sources = cmp.config.sources({
---         { name = "lazydev" },
---         { name = "nvim_lsp" },
---         { name = "path" },
---       }, {
---         { name = "buffer" },
---       }),
---       experimental = {
---         -- only show ghost text when we show ai completions
---         ghost_text = vim.g.ai_cmp and {
---           hl_group = "CmpGhostText",
---         } or false,
---       },
---       sorting = defaults.sorting,
---     }
---   end,
---   main = "lazyvim.util.cmp",
--- }
+    -- Unset custom prop to pass blink.cmp validation
+    opts.sources.compat = nil
+
+    -- check if we need to override symbol kinds
+    for _, provider in pairs(opts.sources.providers or {}) do
+      ---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
+      if provider.kind then
+        local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+        local kind_idx = #CompletionItemKind + 1
+
+        CompletionItemKind[kind_idx] = provider.kind
+        ---@diagnostic disable-next-line: no-unknown
+        CompletionItemKind[provider.kind] = kind_idx
+
+        ---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
+        local transform_items = provider.transform_items
+        ---@param ctx blink.cmp.Context
+        ---@param items blink.cmp.CompletionItem[]
+        provider.transform_items = function(ctx, items)
+          items = transform_items and transform_items(ctx, items) or items
+          for _, item in ipairs(items) do
+            item.kind = kind_idx or item.kind
+          end
+          return items
+        end
+
+        -- Unset custom prop to pass blink.cmp validation
+        provider.kind = nil
+      end
+    end
+
+    require("blink.cmp").setup(opts)
+  end,
+}
